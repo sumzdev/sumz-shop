124c00ca2d79ed2e43a1d9e1253b1d8338ffb189
sunhwa park <sumz.ko@gmail.com>
2022-12-08 13:49:23 +0900

    enter page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,38 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 
 import { SessionProvider } from "next-auth/react";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#084d62",
+    },
+    secondary: {
+      main: "#d2d2d2",
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#084d62",
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={lightTheme}>
       <Head>
         <title>Sumz Shop</title>
       </Head>
       <SessionProvider session={pageProps.session}>
         <Component {...pageProps} />
       </SessionProvider>
-    </>
+    </ThemeProvider>
   );
 }
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,33 +1,94 @@
+import {
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Enter() {
   const { data: session, status } = useSession();
 
   return (
-    <div className="p-10">
-      <div className="mb-10 border-solid border">
-        <p>Status : {status}</p>
-        <p>email : {session?.user?.email}</p>
-        <p>name : {session?.user?.name}</p>
-      </div>
+    <Container className="flex items-center w-full justify-center">
+      <Box className="mt-8 items-center flex flex-row justify-center py-14 gap-3 ">
+        <Image src="/sumz.svg" alt="Sumz Logo" width={72} height={72} />
+        <h2 className="text-4xl font-bold text-center  ">SUMZ</h2>
+      </Box>
 
-      {!session && (
-        <button
-          className="bg-slate-300 px-10 py-3 rounded-md mr-5"
-          onClick={() => signIn("google")}
-        >
-          Google Login
-        </button>
-      )}
+      <Box className="flex flex-col items-center gap-4 md:w-3/4 lg:w-1/2 mx-auto">
+        <TextField
+          fullWidth
+          required
+          id="email"
+          label="이메일"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          fullWidth
+          required
+          id="password"
+          label="비밀번호"
+          name="password"
+          type="password"
+          autoComplete="current-password"
+        />
 
-      {!session?.user && (
-        <button
-          className="bg-slate-300 px-10 py-3 rounded-md"
-          onClick={() => signOut()}
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, py: 1.7 }}
         >
-          SignOut
-        </button>
-      )}
-    </div>
+          로그인
+        </Button>
+
+        <Button className="self-end">
+          <Link href="#">회원가입</Link>
+        </Button>
+
+        <Box className="flex flex-row w-full gap-4 mt-10">
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ py: 1.2 }}
+            onClick={() => signIn("google")}
+          >
+            Google 로그인
+          </Button>
+          <Button
+            variant="contained"
+            fullWidth
+            onClick={() => signIn("google")}
+          >
+            Google 로그인
+          </Button>
+        </Box>
+      </Box>
+
+      <Box className="mt-10">
+        {!session?.user && (
+          <button
+            className="bg-slate-300 px-10 py-3 rounded-md"
+            onClick={() => signOut()}
+          >
+            Google SignOut
+          </button>
+        )}
+
+        <div className="mb-10 border-solid border">
+          <p>Status : {status}</p>
+          <p>email : {session?.user?.email}</p>
+          <p>name : {session?.user?.name}</p>
+        </div>
+      </Box>
+    </Container>
   );
 }
